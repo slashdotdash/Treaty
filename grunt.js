2305ac17804d6c00ddfd42d0cf8f4a955861bcc2
Ben Smith <ben@slashdotdash.net>
2013-01-29 07:53:40 +0000

    Initial Grunt configuration and exclude local npm packages from git.

---
+++ grunt.js
@@ -0,0 +1,72 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+	typescript: {
+      base: {
+        src: ['src/**/*.ts'],
+        dest: 'src/bin/treaty.js',
+        options: {
+          module: 'amd', //or commonjs
+          target: 'es5', //or es3
+          base_path: 'src',
+          sourcemap: true,
+          declaration: true
+        }
+      }
+    },
+    lint: {
+      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    test: {
+      files: ['test/**/*.js']
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint test'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true
+      },
+      globals: {}
+    },
+    uglify: {}
+  });
+
+  grunt.loadNpmTasks('grunt-typescript');
+  
+  // Default task.
+  //grunt.registerTask('default', 'lint test concat min');
+  grunt.registerTask('default', 'typescript');
+};
